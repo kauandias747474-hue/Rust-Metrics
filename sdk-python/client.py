37b54dcbdc915dc047b23a40d00df996e532b785
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-21 13:24:59 -0300

    Update client.py

--- sdk-python/client.py
+++ sdk-python/client.py
@@ -1,1 +1,22 @@
 
+import grpc
+import telemetry_pb2
+import telemetry_pb2_grpc
+
+def run():
+ 
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = telemetry_pb2_grpc.MetricsIngestorStub(channel)
+        
+     
+        request = telemetry_pb2.MetricRequest(
+            sensor_id="PYTHON_SENSOR_01", 
+            value=24.5
+        )
+        
+      
+        response = stub.IngestMetric(request)
+        print(f"Resposta do Motor Rust: {response.message}")
+
+if __name__ == '__main__':
+    run()
