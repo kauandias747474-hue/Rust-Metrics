740f7aeb11e64b6e820267497dc22c8d460046de
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-22 11:25:11 -0300

    Update codec.rs

--- src/engine/codec.rs
+++ src/engine/codec.rs
@@ -1,1 +1,8 @@
+pub struct EngineCodec;
 
+impl EngineCodec {
+     pub fn encode_for_native(data: &str) -> Vec<u8> {
+        println!(" Codec: Preparando dados para o núcleo nativo...");
+        data.as_bytes().to_vec()
+    }
+}
