9c0979b875d070b94628b22da413769305871725
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-23 08:35:43 -0300

    Update processor.h

--- native-core/include/processor.h
+++ native-core/include/processor.h
@@ -1,1 +1,22 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef struct {
+    float std_dev;
+    float mean;
+    int count;
+} ProcessedResult;
+
+
+ProcessedResult process_telemetry_data(const float* data, int size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
