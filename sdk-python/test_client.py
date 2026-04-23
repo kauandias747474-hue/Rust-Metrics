77b4ade354309c90c7614ba29f712a22fc5e033d
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-23 09:24:36 -0300

    Create test_client.py

---
+++ sdk-python/test_client.py
@@ -0,0 +1,30 @@
+import grpc
+import telemetry_pb2
+import telemetry_pb2_grpc
+import time
+
+def run():
+   
+    with grpc.insecure_channel('127.0.0.1:50051') as channel:
+        stub = telemetry_pb2_grpc.MetricsIngestorStub(channel)
+        
+ 
+    
+        dados = [10.0, 20.0, 30.0]
+        
+        def generate_metrics():
+            for val in dados:
+                yield telemetry_pb2.MetricRequest(
+                    sensor_id="SENSOR_TESTE",
+                    value=val,
+                    timestamp=int(time.time())
+                )
+
+        # Envia o stream para o Rust/C++
+        response = stub.SendMetricsStream(generate_metrics())
+        
+        print(f" Resposta do Servidor: {response.message}")
+        print(f" Desvio Padrão (C++): {response.calculated_std_dev}")
+
+if __name__ == '__main__':
+    run()
